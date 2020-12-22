546a371efe666329bbadce22bbf25321d1f014fe
thrashedbrain <47290965+thrashedbrain@users.noreply.github.com>
2020-12-22 11:38:16 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -11,22 +11,17 @@ import java.util.List;
 public class Main {
 
         public static void main(String[] args) {
- try{
-     Document doc = Jsoup.connect("http://edu.sfu-kras.ru/timetable?group=РФ16-33Б").get();
-
-
-     Element Table = doc.select("table").get(1); //Выбираем таблицу с расписанием
-     Elements Rows = Table.select("tr");//Выбираем строки в таблице
-     boolean chetweek = true;
-     int daycounter = 0;
-     String Stringvar = "";
-     String Day = "";
-        System.out.print("Расписание  ебать!\n\n");
-
+            
+            try{
+                Document doc = Jsoup.connect("http://edu.sfu-kras.ru/timetable?group=РФ16-33Б").get();
+                Element Table = doc.select("table").get(1); //Выбираем таблицу с расписанием
+                Elements Rows = Table.select("tr");//Выбираем строки в таблице
+                boolean chetweek = true;
+                int daycounter = 0;
+                String Stringvar = "";
+                String Day = "";
+                
                     for (int i = 0; i < Rows.size(); i++){
-
-
-
 
                         Element row = Rows.get(i); //по номеру индекса получает строку
                         Elements cols = row.select("td");// разбиваем полученную строку по тегу  на столбы
@@ -64,7 +59,8 @@ public class Main {
                         }
 
                         if (chetweek){
-                            if (cols.size() < 4){ Stringvar = cols.get(2).select("b").text() + cols.get(2).ownText();
+                            if (cols.size() < 4){ 
+                                Stringvar = cols.get(2).select("b").text() + cols.get(2).ownText();
                                 System.out.print("     ");
                                 System.out.print(Day);
                                 System.out.print("   ");
@@ -74,13 +70,11 @@ public class Main {
                                 System.out.print("   ");
                                 System.out.print(Stringvar);
                                 System.out.print("            ");
-                                System.out.print(cols.get(2).select("a").text()); //препод и аудитория
+                                System.out.print(cols.get(2).select("a").text()); //преподаватель и аудитория
                                 System.out.print("\n");
-
-
-                            continue;}
+                                continue;}
+                            
                             if (!cols.get(3).hasText()) {continue;}
-
 
                             System.out.print("     ");
                             System.out.print(Day);
@@ -89,12 +83,14 @@ public class Main {
                             System.out.print("   ");
                             System.out.print(cols.get(1).text()); // Время
                             System.out.print("   ");
-                            if (cols.size()< 4){System.out.print(Stringvar);} else {
+                            if (cols.size()< 4){
+                                System.out.print(Stringvar);
+                            } else {
                                 System.out.print(cols.get(3).select("b").text() + cols.get(3).ownText()); //Название и лекция, практика и т.д.
                             }
 
                             System.out.print("            ");
-                            System.out.print(cols.get(3).select("a").text()); //препод и аудитория
+                            System.out.print(cols.get(3).select("a").text()); //преподаватель и аудитория
                             System.out.print("\n");
 
                         } else {
@@ -111,7 +107,7 @@ public class Main {
 
 
                         System.out.print("            ");
-                        System.out.print(cols.get(2).select("a").text()); //препод и аудитория
+                        System.out.print(cols.get(2).select("a").text()); //преподаватель и аудитория
                         System.out.print("\n");}
 
                     }
